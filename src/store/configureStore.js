036f9f8fe1375c56cc4e172506b21fa910bd5ada
Shiv <mail@gmail.com>
2024-01-21 11:04:21 +0530

    Cofigured devToolsEnchancer for Trace using Redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,7 @@
-import {legacy_createStore as createStore, applyMiddleware } from "redux";
-// import { thunk } from "redux-thunk";
+import {legacy_createStore as createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./tasks";
 
-const store = createStore(reducer, 
-    // applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer,devToolsEnhancer({ trace: true}) );
 
 export default store;
